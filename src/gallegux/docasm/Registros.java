bff1c14a928e54b097a2476da1bfbba2135b3202
Fran <gallegux@hotmail.com>
2021-12-11 18:16:02 +0100

    se añaden registros

--- src/gallegux/docasm/Registros.java
+++ src/gallegux/docasm/Registros.java
@@ -5,7 +5,14 @@ package gallegux.docasm;
 public class Registros 
 {
 	
-	private static String[] REGISTROS = {"a", "bc", "b", "c", "de", "d", "e", "hl", "h", "l", "sp"};
+	private static String[] REGISTROS = {"a", 
+			"bc", "b", "c", 
+			"de", "d", "e", 
+			"hl", "h", "l", 
+			"sp",
+			"ix", "ixh", "ixl",
+			"iy", "iyh", "iyl",
+			};
 	
 	
 	public static String test(String s)
